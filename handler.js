00fabef739e191b36f6831d68c38362d5c060051
MynahMarie <mynah.marie@gmail.com>
2017-11-27 16:24:45 +0200

    Create a image route to retrieve random images

--- handler.js
+++ handler.js
@@ -1,37 +1,45 @@
 const fs = require('fs');
 const path = require('path');
-const req = require('request');
+const request = require('request');
+const url = "https://api.unsplash.com/photos/random?client_id=200b4a1dd00e4c89258ce479eccf61ae1e1eb65ca12ef7712f1994cadb579658";
 
-const options = {
-  url: "https://picsum.photos/200/300/?random",
-  method: "GET"
-}
+//authorization code 46c897da7262c3ac97db1e0e258db4d9fb7894bb5dc9bd39e4ca2a32202bf5f6
 
-function handler(request, response) {
+function handler(req, res) {
 
-  let url = request.url;
+  let path = req.url;
 
-  if (url === '/') {
-    response.writeHead(200, {'content-type': 'text/html'});
+  if (path === '/') {
+    res.writeHead(200, {'content-type': 'text/html'});
     fs.readFile('./index.html', function(error, file) {
       if (error) {
         console.log(error);
-        fs.writeHead(404, {'content-type': 'text/html'});
-        response.end("<h1>FILE NoT fOUnd</h1>");
+        res.writeHead(404, {'content-type': 'text/html'});
+        res.end("<h1>FILE NoT fOUnd</h1>");
       }
       else {
-        response.end(file);
+        res.end(file);
       }
     })
   }
 
-  else if (url === '/image') {
-    req.get(options);
+  else if (path === '/image') {
+    let content;
+    let image_url;
+
+    request(url, (error, response, body) => {
+      content = JSON.parse(body);
+      image_url = content.urls.regular;
+      console.log(image_url);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(`<img src=${image_url}></img>`);
+    });
+    // console.log(image_url);
   }
-  
+
   else {
-    fs.writeHead(404, {'content-type': 'text/html'});
-    response.end("<h1>FILE NoT fOUnd</h1>");
+    res.writeHead(404, {'content-type': 'text/html'});
+    res.end("<h1>FILE NoT fOUnd</h1>");
   }
 }
 
